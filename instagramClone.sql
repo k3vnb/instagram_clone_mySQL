a5a27a7e4b30d384b11f0cf28e83f239d7e467db
Kevin Boyle <papershack@gmail.com>
2018-12-11 13:33:12 -0800

    add tables

--- instagramClone.sql
+++ instagramClone.sql
@@ -20,3 +20,32 @@ CREATE TABLE photos (
     created_at TIMESTAMP,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
+
+INSERT INTO photos(image_url, user_id) VALUES
+('/alsked78', 1),
+('/alskee79', 2),
+('/alskef80', 2);
+
+CREATE TABLE comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+INSERT INTO comments(comment_text, user_id, photo_id) VALUES
+('Meow!', 1, 2),
+('Nice!', 3, 2),
+('Super Cool!', 2, 1);
+
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
